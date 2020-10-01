fe5a3f2da65185451fa6b77d15fe106885f79fe8
Habatoo <habatoo@gmail.com>
2020-10-01 22:20:59 +0700

    add calendar, garden, wiki, notes pages

--- app/src/main/java/com/alenkinogorod/MainActivity.java
+++ app/src/main/java/com/alenkinogorod/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,17 +14,26 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextName;
     private EditText editTextPassword;
 
+    private Button buttonName;
+
+    String name;
+    String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editTextName = findViewById(R.id.editTextLogin);
         editTextPassword = findViewById(R.id.editTextPassword);
+
+        
     }
 
     public void onClickEnter(View view) {
-        String name = editTextName.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
+
+        name = editTextName.getText().toString().trim();
+        password = editTextPassword.getText().toString().trim();
+
         if (!name.isEmpty() && !password.isEmpty()) {
             Intent intent = new Intent(this, EnterActivity.class);
             intent.putExtra("name", name);
@@ -32,8 +42,14 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Bce поля должны быть заполнены!", Toast.LENGTH_SHORT).show();
         }
+
     }
 
+    /**
+     * Go to register page method
+     * Input data are username, email and password
+     * @param view
+     */
     public void onClickRegister(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
