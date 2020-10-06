749d507c7bd7e98d4335ff894d21c7bdede9bf27
Habatoo <habatoo@gmail.com>
2020-10-06 20:22:56 +0700

    add site navi add site, wiki, notes calendar pages

--- app/src/main/java/com/alenkinogorod/PersonalCabinetActivity.java
+++ app/src/main/java/com/alenkinogorod/PersonalCabinetActivity.java
@@ -2,7 +2,9 @@ package com.alenkinogorod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PersonalCabinetActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class PersonalCabinetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_cabinet);
+    }
+
+    public void onClickNotes(View view) {
+        Intent intent = new Intent(this, NoteActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickWiki(View view) {
+        Intent intent = new Intent(this, WikiActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickOurSite(View view) {
+        Intent intent = new Intent(this, OurSiteActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickCalendar(View view) {
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
     }
 }
